2b9cad88abf639d639dc60bf66e2c754cda954b2
Noverdi Ramadhan <noverdy123@gmail.com>
2021-12-13 22:24:22 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bang Esan
+
+https://noverdy.github.io/bang-esan/
+
+Bang Esan is a multi-purpose Discord bot for you to listen to music, play some minigames, and more.
+
+## Features
+
+1. Play music on your Discord server
+2. Have fun with minigames
+3. And so on!
+
+Open the website to learn more about Bang Esan.
---
+++ js/script.js
@@ -0,0 +1,42 @@
+document.getElementById('invite-button').onclick = () => {
+	window.open(
+		'https://discord.com/api/oauth2/authorize?client_id=846364862204084274&permissions=2151148608&scope=bot'
+	)
+}
+
+const logo = document.getElementById('logo')
+var logoLock = false
+logo.onclick = () => {
+	if (!logoLock) {
+		logoLock = true
+		logo.classList.add('logo-active')
+		setTimeout(() => {
+			logo.classList.remove('logo-active')
+			logoLock = false
+		}, 1000)
+	}
+}
+
+window.onload = () => {
+	const navbar = document.getElementById('navbar')
+	const paddingVertical = 20
+	document.querySelectorAll('.section').forEach((section) => {
+		section.style.paddingTop = `calc(${navbar.offsetHeight}px + ${paddingVertical}px)`
+		section.style.paddingBottom = `${paddingVertical}px`
+		section.style.height = '100vh'
+	})
+}
+
+window.onscroll = () => {
+	const navbar = document.getElementById('navbar')
+	if (
+		document.documentElement.scrollTop <
+		document.getElementById('logo').offsetTop
+	) {
+		navbar.style.backdropFilter = 'none'
+		navbar.style.backgroundColor = 'transparent'
+	} else {
+		navbar.style.backdropFilter = 'blur(10px)'
+		navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+	}
+}
